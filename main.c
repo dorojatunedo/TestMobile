535f2a1a3868906aa28ba92d7d19cb925d9a6819
dorojatunedo <edorojatun@gmail.com>
2017-11-29 22:31:46 +0700

    kodingan awal

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char laptop[1000]="bentuk\t=\tkotak\nwarna\t=\tmacam2\nbahan\t=\tmacam2\njumlah kaki\t=\t0";
+    char a[100];
+    printf("masukan kata benda = ");
+    scanf("%s",a);
+    printf("%s",laptop);
+    return 0;
+}
